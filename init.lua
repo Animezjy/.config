7ff5659f5b6fbfad2b9769f27c78125ba350f8da
zhangjiyou <zhangjiyou@topjoy.com>
2023-05-17 17:38:35 +0800

    新增插件

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
--- 基础设置
-require('basic')
-require('keybinding')
+-- 基础配置
+require("basic")
+-- Packer插件管理
+require("plugins")
+-- 快捷键映射
+require("keybindings")
+-- 主题设置 （新增）
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+-- 内置LSP
+require("lsp.setup")
